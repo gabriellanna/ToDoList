775adfca12aefdbd38ff5f24159264f5a8ce0870
gabriel <alveslanna97@gmail.com>
2023-01-30 17:59:12 -0300

    Editando homePage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import { Container, Header } from './styles';
+import { Container, Content, Header } from './styles';
+import Img from "../../assets/todo-branco.png"
+import Img2 from "../../assets/empty-data.svg"
+import Button from '@mui/material/Button';
 
 const HomePage = () => {
   return (
     <>
       <Header>
-        <div>HomePage</div>
+        <h1>Lista de Tarefas</h1>
+        <img alt='Imagem de uma lista de tarefas' src={Img} />
       </Header>
       <Container>
-
+        <Content>
+          <div>
+            <Button variant="contained">Logar</Button>
+          </div>
+        </Content>
       </Container>
     </>
+
   )
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,16 +1,60 @@
 import styled from "styled-components";
+import Img2 from "../../assets/empty-data.svg"
 
 export const Header = styled.div`
     width: 100%;
-    height: 80px;
+    height: 8vh;
+
+    display: flex;
+    align-items: center;
 
     background-color: #333;
     color: #fff;
+
+    h1 {
+        margin-left: 70px;
+    }
+
+    img {
+        width: 50px;
+        height: 50px;
+        margin-left: 10px;
+        border-radius: 10px;
+    }
 `;
 
 export const Container = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 92vh;
 
     background-color: #E9E3CE;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Content = styled.div`
+    width: 75%;
+    height: 90%;
+    background-color: rgba( 0, 0, 0, 0.1);
+    border-radius: 50px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin: 30px;
+    background-image: url(${Img2});
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 80%;
+
+
+    div{
+        width: 50%;
+
+        display: flex;
+        flex-direction: column;
+    }
 `;
